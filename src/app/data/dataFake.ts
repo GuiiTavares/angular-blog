91ba244d1f6c8736516463cbc24c6919f237caeb
unknown <guiba18@live.com>
2023-05-23 22:15:56 -0300

    envio final

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,33 @@
+export const dataFake = [
+  {
+    id: '0',
+    title: 'Saiu a nova versão do angular',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    photoCover:
+      'https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder-300x150.png',
+  },
+  {
+    id: '1',
+    title: 'NOVO HOMEM DE FERRO EM 3D',
+    description: 'Marvel anuncia um novo filme do homem de ferro, confira.',
+    photoCover:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREpVFZanWXSH6b5p-CSuXaltsenvQrGnRiWp-zbPv1&s',
+  },
+  {
+    id: '2',
+    title: 'Nova série',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    photoCover:
+      'https://files.meiobit.com/wp-content/uploads/2023/01/the-last-of-us-001.jpg',
+  },
+  {
+    id: '3',
+    title: 'Nova série',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    photoCover:
+      'https://files.meiobit.com/wp-content/uploads/2023/01/the-last-of-us-001.jpg',
+  },
+];
